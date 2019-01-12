649aa1b1baaa3d57503435f32003fc22155005b4
cjhD <cj@hybridata.us>
2019-01-11 21:28:45 -0800

    adding more workshop configs and scripts

---
+++ pyHandler/create_es_meta.py
@@ -0,0 +1,20 @@
+import os
+from elasticsearchquery import ElasticSearchQuery
+
+
+def handler(event, context):
+    esQuery = esQueryFactory()
+    esQuery.run()
+
+
+def esQueryFactory():
+    return ElasticSearchQuery(
+        es_endpoint=os.environ['ES_ENDPOINT'],
+        index_name=os.environ['ES_INDEX'],
+        query_file=os.environ['ES_IOT_MAPPING_FILE'],
+        region=os.environ['ES_REGION'],
+    )
+
+
+if __name__ == '__main__':
+    handler({}, {})
